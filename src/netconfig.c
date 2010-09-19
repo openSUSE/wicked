0b7cb6d3c4e3b10148ecbbf64f080dc084a45c93
Olaf Kirch <okir@suse.de>
2010-09-19 12:15:13 +0200

    Some cleanups in netconfig handle
    When creating a netconfig handle, error out when the syntax object
    is NULL. This is no longer needed or supported.
    
    Rename some functions from generic __ni_foobar to __ni_netconfig_foobar
    
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- src/netconfig.c
+++ src/netconfig.c
@@ -1,5 +1,6 @@
 /*
  * Routines for loading and storing all network configuration
+ * from system config files.
  *
  * Copyright (C) 2009-2010 Olaf Kirch <okir@suse.de>
  */
@@ -12,12 +13,12 @@
 #include "config.h"
 
 static const char *	ni_netconfig_default_schema(const char *);
-static int		__ni_config_refresh(ni_handle_t *nih);
-static void		__ni_config_close(ni_handle_t *nih);
+static int		__ni_netonfig_refresh(ni_handle_t *nih);
+static void		__ni_netonfig_close(ni_handle_t *nih);
 
-static struct ni_ops ni_config_ops = {
-	.refresh	= __ni_config_refresh,
-	.close		= __ni_config_close,
+static struct ni_ops ni_netconfig_ops = {
+	.refresh	= __ni_netonfig_refresh,
+	.close		= __ni_netonfig_close,
 };
 
 ni_handle_t *
@@ -26,12 +27,11 @@ ni_netconfig_open(ni_syntax_t *syntax)
 	ni_handle_t *nih;
 
 	if (!syntax) {
-		syntax = ni_netconfig_default_syntax(NULL);
-		if (!syntax)
-			return NULL;
+		ni_error("ni_netconfig_open: syntax is NULL");
+		return NULL;
 	}
 
-	nih = __ni_handle_new(&ni_config_ops);
+	nih = __ni_handle_new(&ni_netconfig_ops);
 	if (nih)
 		nih->default_syntax = syntax;
 
@@ -41,19 +41,19 @@ ni_netconfig_open(ni_syntax_t *syntax)
 ni_syntax_t *
 ni_netconfig_default_syntax(const char *root_dir)
 {
+	const char *schema, *base_path = NULL;
 	ni_syntax_t *syntax;
-	const char *schema;
 
 	schema = ni_global.config->default_syntax;
 	if (schema) {
-		//base_path = ni_global.config->default_syntax_path;
+		base_path = ni_global.config->default_syntax_path;
 	} else {
 		/* No syntax defined in configuration file. Try to find out
 		 * which distro we're on. */
 		schema = ni_netconfig_default_schema(root_dir);
 	}
 
-	syntax = ni_syntax_new(schema, NULL);
+	syntax = ni_syntax_new(schema, base_path);
 	if (root_dir)
 		ni_syntax_set_root_directory(syntax, root_dir);
 	return syntax;
@@ -88,17 +88,17 @@ ni_netconfig_default_schema(const char *root_dir)
 }
 
 static void
-__ni_config_close(ni_handle_t *nih)
+__ni_netonfig_close(ni_handle_t *nih)
 {
 	if (nih->default_syntax)
 		ni_syntax_free(nih->default_syntax);
 }
 
 static int
-__ni_config_refresh(ni_handle_t *nih)
+__ni_netonfig_refresh(ni_handle_t *nih)
 {
 	if (nih->default_syntax == NULL) {
-		error("__ni_config_refresh: no syntax object associated");
+		ni_error("netonfig: cannot refresh, no syntax associated");
 		return -1;
 	}
 	return ni_syntax_parse_all(nih->default_syntax, nih);
