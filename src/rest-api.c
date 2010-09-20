openSUSE/wicked

commit c07890b7e3158757ef6fe61ab8460a911b64a1d1

Change struct ni_wicked_request to hold XML as xml_nodes rather than xml_documents
This will allow us to unify client and server side rest api even further

src/rest-api.c
@@ -19,6 +19,7 @@
 #include <wicked/logging.h>
 #include <wicked/xml.h>
 #include <wicked/xpath.h>
+#include "netinfo_priv.h"
 
 int
 ni_rest_request_process(ni_wicked_request_t *req, const char *cmd, const char *path)
@@ -101,15 +102,15 @@ generic_interface_response(ni_handle_t *nih, ni_interface_t *ifp, ni_wicked_requ
 	ni_syntax_t *xmlsyntax = ni_default_xml_syntax();
 
 	if (ifp == NULL) {
-		req->xml_out = ni_syntax_xml_from_all(xmlsyntax, nih);
-	} else {
-		xml_node_t *result;
+		xml_document_t *doc;
 
-		result = ni_syntax_xml_from_interface(xmlsyntax, nih, ifp);
-		if (result) {
-			req->xml_out = xml_document_new();
-			xml_document_set_root(req->xml_out, result);
+		doc = ni_syntax_xml_from_all(xmlsyntax, nih);
+		if (doc) {
+			req->xml_out = xml_document_take_root(doc);
+			xml_document_free(doc);
 		}
+	} else {
+		req->xml_out = ni_syntax_xml_from_interface(xmlsyntax, nih, ifp);
 	}
 
 	if (req->xml_out == NULL) {
@@ -173,7 +174,7 @@ generic_interface_put(ni_handle_t *nih, const char *ifname, ni_wicked_request_t 
 		goto failed;
 	}
 
-	if (ni_syntax_xml_to_all(ni_default_xml_syntax(), cnih, req->xml_in) < 0) {
+	if (__ni_syntax_xml_to_all(ni_default_xml_syntax(), cnih, req->xml_in) < 0) {
 		werror(req, "unable to parse interface configuration");
 		goto failed;
 	}
@@ -185,7 +186,7 @@ generic_interface_put(ni_handle_t *nih, const char *ifname, ni_wicked_request_t 
 
 	/* Find the XML intrface element - we want to pass it to the configure
 	 * routine. This helps us write flexible extensions */
-	for (cfg_xml = req->xml_in->root->children; cfg_xml; cfg_xml = cfg_xml->next) {
+	for (cfg_xml = req->xml_in->children; cfg_xml; cfg_xml = cfg_xml->next) {
 		const char *name;
 
 		if (strcmp(cfg_xml->name, "interface"))
@@ -285,7 +286,6 @@ static int
 system_hostname_get(const char *path, ni_wicked_request_t *req)
 {
 	char hostname[256];
-	xml_node_t *hnode;
 
 	if (path && *path) {
 		werror(req, "excess elements in path");
@@ -297,10 +297,7 @@ system_hostname_get(const char *path, ni_wicked_request_t *req)
 		return 0;
 	}
 
-	req->xml_out = xml_document_new();
-	hnode = xml_node_new("hostname", xml_document_root(req->xml_out));
-	xml_node_set_cdata(hnode, hostname);
-
+	req->xml_out = xml_node_new("hostname", NULL);
 	return 0;
 }
 

include/wicked/wicked.h
@@ -20,8 +20,8 @@ typedef struct ni_wicked_request {
 	int		op;
 
 	char *		error_msg;
-	xml_document_t *xml_in;
-	xml_document_t *xml_out;
+	xml_node_t *	xml_in;
+	xml_node_t *	xml_out;
 } ni_wicked_request_t;
 
 typedef int (*ni_rest_handler_t)(const char *, ni_wicked_request_t *req);

src/netinfo_priv.h
@@ -80,6 +80,7 @@ extern int		__ni_system_interface_configure(ni_handle_t *, ni_interface_t *, xml
 extern int		__ni_system_interface_delete(ni_handle_t *, const char *);
 extern int		__ni_rtevent_refresh_all(ni_handle_t *);
 
+extern int		__ni_syntax_xml_to_all(ni_syntax_t *, ni_handle_t *, xml_node_t *);
 extern ni_syntax_t *	__ni_syntax_sysconfig_suse(const char *pathname);
 extern ni_syntax_t *	__ni_syntax_sysconfig_redhat(const char *pathname);
 extern ni_syntax_t *	__ni_syntax_netcf(const char *pathname);

src/syntax.c
@@ -278,17 +278,24 @@ error:
 int
 ni_syntax_xml_to_all(ni_syntax_t *syntax, ni_handle_t *nih, xml_document_t *doc)
 {
-	xml_node_t *root, *child;
+	if (!doc)
+		return -1;
+	return __ni_syntax_xml_to_all(syntax, nih, doc->root);
+}
+
+int
+__ni_syntax_xml_to_all(ni_syntax_t *syntax, ni_handle_t *nih, xml_node_t *root)
+{
+	xml_node_t *child;
 
 	if (!syntax->xml_to_interface) {
 		error("%s: syntax not capable of creating interface from xml", __FUNCTION__);
 		return -1;
 	}
 
-	if (!doc)
+	if (!root)
 		return -1;
 
-	root = xml_document_root(doc);
 	for (child = root->children; child; child = child->next) {
 		if (strcmp(child->name, "interface"))
 			continue;

wickedd.c
@@ -218,7 +218,7 @@ wicked_process_network_restcall(int fd)
 	}
 
 	/* Now get the XML document, if any */
-	req.xml_in = xml_document_scan(sock);
+	req.xml_in = xml_node_scan(sock);
 	if (req.xml_in == NULL) {
 		werror(&req, "unable to parse xml document");
 		goto error;
@@ -227,7 +227,7 @@ wicked_process_network_restcall(int fd)
 	if (ni_rest_request_process(&req, cmd, path) >= 0) {
 		fprintf(sock, "OK\n");
 		if (req.xml_out)
-			xml_document_print(req.xml_out, sock);
+			xml_node_print(req.xml_out, sock);
 	} else {
 error:
 		if (req.error_msg == NULL) {
@@ -239,9 +239,9 @@ error:
 
 	fflush(sock);
 	if (req.xml_in)
-		xml_document_free(req.xml_in);
+		xml_node_free(req.xml_in);
 	if (req.xml_out)
-		xml_document_free(req.xml_out);
+		xml_node_free(req.xml_out);
 	ni_string_free(&req.error_msg);
 }
 

commit d36a24e56a3576187f0eaee4882e4c13cfe3ccc5

Fix handling of GET /system/hostname
It seems this was broken quite badly by commit c07890b7.
We would return an empty <hostname> element, and fail to
report errors to the client.

Signed-off-by: Olaf Kirch <okir@suse.de>

src/rest-api.c
@@ -431,15 +431,16 @@ system_hostname_get(const char *path, ni_wicked_request_t *req)
 
 	if (path && *path) {
 		werror(req, "excess elements in path");
-		return 0;
+		return -1;
 	}
 
 	if (gethostname(hostname, sizeof(hostname)) < 0) {
 		werror(req, "error getting hostname");
-		return 0;
+		return -1;
 	}
 
 	req->xml_out = xml_node_new("hostname", NULL);
+	xml_node_set_cdata(req->xml_out, hostname);
 	return 0;
 }
 