acbd33eeb4716a87b49e9f422d0fc42cb22017f5
Olaf Kirch <okir@suse.de>
2010-09-23 23:55:27 +0200

    Add dhcp debugging
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- include/wicked/logging.h
+++ include/wicked/logging.h
@@ -27,6 +27,7 @@ enum {
 	NI_TRACE_EXTENSION	= 0x000008,
 	NI_TRACE_WICKED		= 0x000010,
 	NI_TRACE_EVENTS		= 0x000020,
+	NI_TRACE_DHCP		= 0x000040,
 };
 
 extern unsigned int	ni_debug;
@@ -42,5 +43,6 @@ extern unsigned int	ni_debug;
 #define ni_debug_extension(fmt, args...)	__ni_debug(NI_TRACE_EXTENSION, fmt, ##args)
 #define ni_debug_wicked(fmt, args...)		__ni_debug(NI_TRACE_WICKED, fmt, ##args)
 #define ni_debug_events(fmt, args...)		__ni_debug(NI_TRACE_EVENTS, fmt, ##args)
+#define ni_debug_dhcp(fmt, args...)		__ni_debug(NI_TRACE_DHCP, fmt, ##args)
 
 #endif /* __WICKED_LOGGING_H__ */
