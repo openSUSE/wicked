a58590aa2da14ce9e959928033a9080f0559dedd
Olaf Kirch <okir@suse.de>
2010-10-03 21:17:17 +0200

    Add missing wicked/dhcp.h header file
    Signed-off-by: Olaf Kirch <okir@suse.de>

---
+++ include/wicked/dhcp.h
@@ -0,0 +1,12 @@
+/*
+ * netinfo dhcp supplicant
+ *
+ * Copyright (C) 2010 Olaf Kirch <okir@suse.de>
+ */
+
+#ifndef __WICKED_DHCP_H__
+#define __WICKED_DHCP_H__
+
+extern ni_addrconf_t	ni_dhcp_addrconf;
+
+#endif /* __WICKED_DHCP_H__ */
