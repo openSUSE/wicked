d8ef8df34d23989352953679001b55c00b082959
Olaf Kirch <okir@suse.de>
2011-03-29 10:55:01 +0200

    Add new header file util_priv.h
    Signed-off-by: Olaf Kirch <okir@suse.de>

---
+++ src/util_priv.h
@@ -0,0 +1,16 @@
+/*
+ * Internal helper functions.
+ * Do not confuse with <wicked/util.h> which is public.
+ *
+ * Copyright (C) 2010 Olaf Kirch <okir@suse.de>
+ */
+
+#ifndef __WICKED_UTIL_PRIV_H__
+#define __WICKED_UTIL_PRIV_H__
+
+extern void *	xcalloc(unsigned int, size_t);
+extern char *	xstrdup(const char *);
+
+#endif /* __WICKED_UTIL_PRIV_H__ */
+
+
