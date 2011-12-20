9bd5142373082bc59eaa508edf117c5e6488bf12
Olaf Kirch <okir@suse.de>
2011-12-20 10:57:23 +0100

    Convert Ethernet interface to use generic attributes
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- src/dbus-objects/ethernet.c
+++ src/dbus-objects/ethernet.c
@@ -26,129 +26,27 @@ static ni_dbus_method_t		wicked_dbus_ethernet_methods[] = {
 
 #define NULL_ether	((ni_ethernet_t *) 0)
 
-static ni_ethernet_t *
-__wicked_dbus_ethernet_handle(const ni_dbus_object_t *object, DBusError *error)
+void *
+ni_objectmodel_get_ethernet(const ni_dbus_object_t *object, DBusError *error)
 {
 	ni_interface_t *ifp = ni_dbus_object_get_handle(object);
 	ni_ethernet_t *eth;
 
 	if (!(eth = ni_interface_get_ethernet(ifp))) {
-		dbus_set_error(error, DBUS_ERROR_FAILED, "Error getting ethernet property");
+		dbus_set_error(error, DBUS_ERROR_FAILED, "Error getting ethernet handle for interface");
 		return NULL;
 	}
 	return eth;
 }
 
-/*
- * property link_speed
- */
-static dbus_bool_t
-__wicked_dbus_ethernet_get_link_speed(const ni_dbus_object_t *object,
-				const ni_dbus_property_t *property,
-				ni_dbus_variant_t *result,
-				DBusError *error)
-{
-	return __ni_objectmodel_get_property_uint(
-			__wicked_dbus_ethernet_handle(object, error),
-			&NULL_ether->link_speed, result);
-}
-
-static dbus_bool_t
-__wicked_dbus_ethernet_set_link_speed(ni_dbus_object_t *object,
-				const ni_dbus_property_t *property,
-				const ni_dbus_variant_t *result,
-				DBusError *error)
-{
-	return __ni_objectmodel_set_property_uint(
-			__wicked_dbus_ethernet_handle(object, error),
-			&NULL_ether->link_speed, result);
-}
-
-/*
- * property port_type
- */
-static dbus_bool_t
-__wicked_dbus_ethernet_get_port_type(const ni_dbus_object_t *object,
-				const ni_dbus_property_t *property,
-				ni_dbus_variant_t *result,
-				DBusError *error)
-{
-	return __ni_objectmodel_get_property_uint(
-			__wicked_dbus_ethernet_handle(object, error),
-			&NULL_ether->port_type, result);
-}
-
-static dbus_bool_t
-__wicked_dbus_ethernet_set_port_type(ni_dbus_object_t *object,
-				const ni_dbus_property_t *property,
-				const ni_dbus_variant_t *result,
-				DBusError *error)
-{
-	return __ni_objectmodel_set_property_uint(
-			__wicked_dbus_ethernet_handle(object, error),
-			&NULL_ether->port_type, result);
-}
-
-/*
- * property duplex
- */
-static dbus_bool_t
-__wicked_dbus_ethernet_get_duplex(const ni_dbus_object_t *object,
-				const ni_dbus_property_t *property,
-				ni_dbus_variant_t *result,
-				DBusError *error)
-{
-	return __ni_objectmodel_get_property_uint(
-			__wicked_dbus_ethernet_handle(object, error),
-			&NULL_ether->duplex, result);
-}
-
-static dbus_bool_t
-__wicked_dbus_ethernet_set_duplex(ni_dbus_object_t *object,
-				const ni_dbus_property_t *property,
-				const ni_dbus_variant_t *result,
-				DBusError *error)
-{
-	return __ni_objectmodel_set_property_uint(
-			__wicked_dbus_ethernet_handle(object, error),
-			&NULL_ether->duplex, result);
-}
-
-/*
- * property autoneg_enable
- */
-static dbus_bool_t
-__wicked_dbus_ethernet_get_autoneg_enable(const ni_dbus_object_t *object,
-				const ni_dbus_property_t *property,
-				ni_dbus_variant_t *result,
-				DBusError *error)
-{
-	return __ni_objectmodel_get_property_uint(
-			__wicked_dbus_ethernet_handle(object, error),
-			&NULL_ether->autoneg_enable, result);
-}
-
-static dbus_bool_t
-__wicked_dbus_ethernet_set_autoneg_enable(ni_dbus_object_t *object,
-				const ni_dbus_property_t *property,
-				const ni_dbus_variant_t *result,
-				DBusError *error)
-{
-	return __ni_objectmodel_set_property_uint(
-			__wicked_dbus_ethernet_handle(object, error),
-			&NULL_ether->autoneg_enable, result);
-}
-
-#define WICKED_ETHERNET_PROPERTY(type, __name, rw) \
-	NI_DBUS_PROPERTY(type, __name, __wicked_dbus_ethernet, rw)
-#define WICKED_ETHERNET_PROPERTY_SIGNATURE(signature, __name, rw) \
-	__NI_DBUS_PROPERTY(signature, __name, __wicked_dbus_ethernet, rw)
+#define ETHERNET_UINT_PROPERTY(dbus_name, member_name, rw) \
+	NI_DBUS_GENERIC_UINT_PROPERTY(ethernet, dbus_name, member_name, rw)
 
 static ni_dbus_property_t	wicked_dbus_ethernet_properties[] = {
-	WICKED_ETHERNET_PROPERTY(UINT32, link_speed, RO),
-	WICKED_ETHERNET_PROPERTY(UINT32, port_type, RO),
-	WICKED_ETHERNET_PROPERTY(UINT32, duplex, RO),
-	WICKED_ETHERNET_PROPERTY(UINT32, autoneg_enable, RO),
+	ETHERNET_UINT_PROPERTY(links-speed, link_speed, RO),
+	ETHERNET_UINT_PROPERTY(port-type, port_type, RO),
+	ETHERNET_UINT_PROPERTY(duplex, duplex, RO),
+	ETHERNET_UINT_PROPERTY(autoneg-enable, autoneg_enable, RO),
 	{ NULL }
 };
 
