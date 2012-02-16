79551cf5ef8f447842b559774aedc22522bd038f
Olaf Kirch <okir@suse.de>
2012-02-16 18:00:00 +0100

    Some objectmodel cleanup
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- src/dbus-objects/model.c
+++ src/dbus-objects/model.c
@@ -218,68 +218,10 @@ ni_objectmodel_get_class(const char *name)
 	return NULL;
 }
 
-/*
- * This method allows clients to create new (virtual) network interfaces.
- * The first argument is the DBus service name of the interface type to
- * create, eg. com.suse.Wicked.Interface.VLAN for a vlan interface.
- * The second argument is a dict containing all the properties making up the
- * configuration of the new interface. These properties must be supported by
- * the chosen service, i.e. when creating a VLAN device, you can only specify
- * VLAN properties, but no, say, network configuration items.
- *
- * The only exception from this rule is the special property "name", which
- * can be used to requests a specific name for the newly created interface.
- */
-static dbus_bool_t
-__ni_dbus_netif_create(ni_dbus_object_t *object, const ni_dbus_method_t *method,
-			unsigned int argc, const ni_dbus_variant_t *argv,
-			ni_dbus_message_t *reply, DBusError *error)
-{
-	const char *interface_name, *object_path;
-	const ni_dbus_service_t *service;
-	ni_dbus_object_t *result;
-	DBusMessageIter iter;
-
-	NI_TRACE_ENTER();
-	if (!ni_dbus_variant_get_string(&argv[0], &interface_name))
-		goto bad_args;
-
-	service = ni_objectmodel_service_by_name(interface_name);
-	if (service == NULL) {
-		dbus_set_error(error, DBUS_ERROR_FAILED,
-			"Unknown dbus interface %s", interface_name);
-		return FALSE;
-	}
-
-	/* FIXME: we should make sure this is compatible with class netif */
-
-	result = ni_objectmodel_new_interface(ni_dbus_object_get_server(object), service, &argv[1], error);
-	if (!result)
-		return FALSE;
-
-	dbus_message_iter_init_append(reply, &iter);
-
-	object_path = ni_dbus_object_get_path(result);
-	dbus_message_iter_append_basic(&iter, DBUS_TYPE_OBJECT_PATH, &object_path);
-
-	return TRUE;
-
-bad_args:
-	dbus_set_error(error, DBUS_ERROR_FAILED,
-			"Bad argument in call to Interface.create()");
-	return FALSE;
-}
-
-static ni_dbus_method_t		wicked_dbus_netif_methods[] = {
-	{ "create",		"sa{sv}",	__ni_dbus_netif_create },
-	{ NULL }
-};
-
-
 static ni_dbus_service_t	ni_objectmodel_netif_list_service = {
-	.name		= WICKED_DBUS_INTERFACE ".Obsolete",
+	.name		= WICKED_DBUS_NETIFLIST_INTERFACE,
 	.compatible	= &ni_objectmodel_netif_list_class,
-	.methods	= wicked_dbus_netif_methods,
+//	.methods	= wicked_dbus_netif_methods,
 //	.properties	= wicked_dbus_netif_properties,
 };
 
