46a96cd762822be775ec2f3b6a6cb955d7fb3b13
Olaf Kirch <okir@suse.de>
2012-02-16 18:00:00 +0100

    Avoid segfault in ni_dbus_object_get_service_for_method by checking for NULL method name
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- src/dbus-object.c
+++ src/dbus-object.c
@@ -268,7 +268,7 @@ ni_dbus_object_get_service_for_method(const ni_dbus_object_t *object, const char
 	const ni_dbus_service_t *svc;
 	unsigned int i;
 
-	if (object->interfaces == NULL)
+	if (object->interfaces == NULL || method == NULL)
 		return NULL;
 
 	for (i = 0; (svc = object->interfaces[i]) != NULL; ++i) {
