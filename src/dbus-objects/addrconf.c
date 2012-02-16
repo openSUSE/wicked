8604791b075039057a9ad38468ed17239c7a47db
Olaf Kirch <okir@suse.de>
2012-02-16 18:00:00 +0100

    Next steps on addrconf: support dhcp4 forwarding
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- src/dbus-objects/addrconf.c
+++ src/dbus-objects/addrconf.c
@@ -27,7 +27,10 @@
 
 
 #define WICKED_DBUS_ADDRCONF_IPV4STATIC_INTERFACE	WICKED_DBUS_INTERFACE ".Addrconf.ipv4.static"
+#define WICKED_DBUS_ADDRCONF_IPV4DHCP_INTERFACE		WICKED_DBUS_INTERFACE ".Addrconf.ipv4.dhcp"
 #define WICKED_DBUS_ADDRCONF_IPV6STATIC_INTERFACE	WICKED_DBUS_INTERFACE ".Addrconf.ipv6.static"
+
+static const char *	ni_objectmodel_dhcp4_object_path(const ni_interface_t *);
 
 /*
  * Interface.acquire(dict options)
@@ -307,6 +310,12 @@ ni_objectmodel_addrconf_ipv6_static_configure(ni_dbus_object_t *object, const ni
 	}
 	dict = &argv[0];
 
+	/* FIXME: what we should do here is
+	 *  - create an empty addrconf request, with no uuid or event ID
+	 *  - create a lease object with the provided addresses and routes
+	 *  - install the lease (ie configure addresses)
+	 */
+
 	req = ni_addrconf_request_new(NI_ADDRCONF_STATIC, AF_INET6);
 	if (!__ni_objectmodel_set_address_dict(&req->statik.addrs, dict, error)
 	 || !__ni_objectmodel_set_route_dict(&req->statik.routes, dict, error))
@@ -331,6 +340,128 @@ ni_objectmodel_addrconf_ipv6_static_configure(ni_dbus_object_t *object, const ni
 }
 
 /*
+ * Forward an addrconf request to a supplicant service, such as DHCP or zeroconf
+ */
+typedef struct ni_dbus_addrconf_forwarder {
+	ni_dbus_client_t *	client;
+	const char *		bus_name;
+	const char *		interface;
+	ni_dbus_class_t		class;
+} ni_dbus_addrconf_forwarder_t;
+
+static dbus_bool_t
+ni_objectmodel_addrconf_forward(ni_dbus_addrconf_forwarder_t *forwarder,
+			const char *method_name, const char *object_path,
+			unsigned int argc, const ni_dbus_variant_t *argv,
+			DBusError *error)
+{
+	ni_dbus_object_t *object;
+
+	if (forwarder->client == NULL) {
+		forwarder->client = ni_create_dbus_client(forwarder->bus_name);
+		if (forwarder->client == NULL) {
+			dbus_set_error(error, "unable to create call forwarder for %s",
+					forwarder->bus_name);
+			return FALSE;
+		}
+
+		ni_dbus_client_add_signal_handler(forwarder->client, NULL, NULL,
+				forwarder->interface,
+				ni_objectmodel_addrconf_signal_handler,
+				NULL);
+	}
+
+	object = ni_dbus_client_object_new(forwarder->client, &forwarder->class,
+			object_path, forwarder->interface, NULL);
+
+	if (!ni_dbus_object_call_variant(object, forwarder->interface, method_name, argc, argv, 0, NULL, error))
+		return FALSE;
+
+	return TRUE;
+}
+
+/*
+ * Configure IPv4 addresses via DHCP
+ */
+static dbus_bool_t
+ni_objectmodel_addrconf_ipv4_dhcp_configure(ni_dbus_object_t *object, const ni_dbus_method_t *method,
+			unsigned int argc, const ni_dbus_variant_t *argv,
+			ni_dbus_message_t *reply, DBusError *error)
+{
+	static ni_dbus_addrconf_forwarder_t forwarder = {
+		.bus_name	= WICKED_DBUS_BUS_NAME_DHCP4,
+		.interface	= WICKED_DBUS_DHCP4_INTERFACE,
+		.class = {
+			.name	= "netif-dhcp-forwarder",
+		}
+	};
+	ni_addrconf_request_t *req = NULL;
+	ni_interface_t *dev;
+	int rv;
+
+	if (!(dev = get_interface(object, error)))
+		return FALSE;
+
+	if (argc != 1 || !ni_dbus_variant_is_dict(&argv[0])) {
+		dbus_set_error(error, DBUS_ERROR_INVALID_ARGS,
+				"%s.%s: exected one dict argument",
+				WICKED_DBUS_ADDRCONF_IPV4DHCP_INTERFACE, method->name);
+		return FALSE;
+	}
+
+	/* FIXME: what we want to do here is
+	 *  - create a request handle, and register it with the interface
+	 *  - assign a uuid
+	 *  - assign an event ID
+	 *  - forward the request, along with the uuid
+	 *  - when we receive a lease event with the matching uuid,
+	 *    broadcast a corresponding interface event (with the assigned
+	 *    even ID)
+	 * Note, we do not record the contents of the addrconf request,
+	 * which may be totally free-form.
+	 */
+	req = ni_addrconf_request_new(NI_ADDRCONF_DHCP, AF_INET);
+#if 0
+	/* register req with interface */
+#endif
+
+	if (!ni_objectmodel_addrconf_forward(&forwarder, "acquire",
+				ni_objectmodel_dhcp4_object_path(dev),
+				argc, argv, error)) {
+		//ni_addrconf_request_free(req);
+		return FALSE;
+	}
+
+	rv = 0;
+	ni_addrconf_request_free(req);
+
+	if (rv < 0) {
+		dbus_set_error(error,
+				DBUS_ERROR_FAILED,
+				"Error forwarding IPv4 DHCP request");
+		return FALSE;
+	} else {
+		/* A NULL event ID tells the caller that we're done, there's no event
+		 * to wait for. */
+		ni_dbus_message_append_uint32(reply, 0);
+	}
+
+	return TRUE;
+}
+
+/*
+ * Get the dhcp4 object path for the device
+ */
+static const char *
+ni_objectmodel_dhcp4_object_path(const ni_interface_t *dev)
+{
+	static char object_path[256];
+
+	snprintf(object_path, sizeof(object_path), WICKED_DBUS_OBJECT_PATH "/DHCP4/Interface/%d", dev->link.ifindex);
+	return object_path;
+}
+
+/*
  * Addrconf methods
  */
 static const ni_dbus_method_t		ni_objectmodel_addrconf_ipv4_static_methods[] = {
@@ -340,6 +471,11 @@ static const ni_dbus_method_t		ni_objectmodel_addrconf_ipv4_static_methods[] = {
 
 static const ni_dbus_method_t		ni_objectmodel_addrconf_ipv6_static_methods[] = {
 	{ "configure",		"a{sv}",		ni_objectmodel_addrconf_ipv6_static_configure },
+	{ NULL }
+};
+
+static const ni_dbus_method_t		ni_objectmodel_addrconf_ipv4_dhcp_methods[] = {
+	{ "configure",		"a{sv}",		ni_objectmodel_addrconf_ipv4_dhcp_configure },
 	{ NULL }
 };
 
@@ -355,3 +491,9 @@ ni_dbus_service_t			ni_objectmodel_addrconf_ipv6_static_service = {
 	.name		= WICKED_DBUS_ADDRCONF_IPV4STATIC_INTERFACE,
 	.methods	= ni_objectmodel_addrconf_ipv6_static_methods,
 };
+
+ni_dbus_service_t			ni_objectmodel_addrconf_ipv4_dhcp_service = {
+	.name		= WICKED_DBUS_ADDRCONF_IPV4DHCP_INTERFACE,
+	.methods	= ni_objectmodel_addrconf_ipv4_dhcp_methods,
+};
+
--- src/dbus-objects/interface.c
+++ src/dbus-objects/interface.c
@@ -39,6 +39,7 @@ static ni_dbus_service_t	ni_objectmodel_netif_list_service;
 static ni_dbus_service_t	ni_objectmodel_netif_service;
 extern ni_dbus_service_t	ni_objectmodel_addrconf_ipv4_static_service;
 extern ni_dbus_service_t	ni_objectmodel_addrconf_ipv6_static_service;
+extern ni_dbus_service_t	ni_objectmodel_addrconf_ipv4_dhcp_service;
 static ni_dbus_property_t	ni_objectmodel_netif_request_properties[];
 
 /*
@@ -89,6 +90,9 @@ ni_objectmodel_register_netif_classes(void)
 
 	ni_objectmodel_addrconf_ipv6_static_service.compatible = &ni_objectmodel_netif_class;
 	ni_objectmodel_register_service(&ni_objectmodel_addrconf_ipv6_static_service);
+
+	ni_objectmodel_addrconf_ipv4_dhcp_service.compatible = &ni_objectmodel_netif_class;
+	ni_objectmodel_register_service(&ni_objectmodel_addrconf_ipv4_dhcp_service);
 }
 
 /*
