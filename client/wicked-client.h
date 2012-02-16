75e557a4e5e541cda890b9c04a201e4405756058
Olaf Kirch <okir@suse.de>
2012-02-16 18:00:00 +0100

    ifup: forgot to include new include file wicked-client.h
    Signed-off-by: Olaf Kirch <okir@suse.de>

---
+++ client/wicked-client.h
@@ -0,0 +1,22 @@
+/*
+ * No REST for the wicked!
+ *
+ * Copyright (C) 2010-2012 Olaf Kirch <okir@suse.de>
+ */
+
+#ifndef WICKED_CLIENT_H
+#define WICKED_CLIENT_H
+
+extern int			do_ifup(int argc, char **argv);
+
+extern ni_dbus_object_t *	wicked_dbus_client_create(void);
+extern ni_dbus_object_t *	wicked_get_interface_object(const char *);
+extern xml_node_t *		wicked_find_link_properties(const xml_node_t *);
+extern const ni_dbus_service_t *wicked_link_layer_factory_service(const char *);
+
+extern char *			wicked_create_interface_xml(const ni_dbus_service_t *,
+					const char *, xml_node_t *);
+extern dbus_bool_t		wicked_link_change_xml(ni_dbus_object_t *, xml_node_t *,
+					unsigned int *);
+
+#endif /* WICKED_CLIENT_H */
