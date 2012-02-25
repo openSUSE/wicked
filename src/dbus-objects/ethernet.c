54f54633559022e5a34c591d5b78f6089f4fb3ae
Olaf Kirch <okir@suse.de>
2012-02-25 12:01:50 +0100

    Extended ethernet schema: added offload info to properties
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- src/dbus-objects/ethernet.c
+++ src/dbus-objects/ethernet.c
@@ -7,6 +7,7 @@
 #include <wicked/netinfo.h>
 #include <wicked/logging.h>
 #include <wicked/ethernet.h>
+#include "dbus-common.h"
 #include "model.h"
 
 #include <wicked/ethernet.h>
@@ -61,6 +62,71 @@ __ni_objectmodel_ethernet_set_address(ni_dbus_object_t *object,
 	return TRUE;
 }
 
+static dbus_bool_t
+__ni_objectmodel_ethernet_get_offload(const ni_dbus_object_t *object,
+				const ni_dbus_property_t *property,
+				ni_dbus_variant_t *result,
+				DBusError *error)
+{
+	ni_ethernet_t *eth;
+	
+	if (!(eth = ni_objectmodel_get_ethernet(object, error)))
+		return FALSE;
+
+	if (eth->offload.rx_csum != NI_ETHERNET_SETTING_DEFAULT)
+		ni_dbus_dict_add_uint32(result, "rx-csum", eth->offload.rx_csum);
+	if (eth->offload.tx_csum != NI_ETHERNET_SETTING_DEFAULT)
+		ni_dbus_dict_add_uint32(result, "tx-csum", eth->offload.tx_csum);
+	if (eth->offload.scatter_gather != NI_ETHERNET_SETTING_DEFAULT)
+		ni_dbus_dict_add_uint32(result, "scatter-gather", eth->offload.scatter_gather);
+	if (eth->offload.tso != NI_ETHERNET_SETTING_DEFAULT)
+		ni_dbus_dict_add_uint32(result, "tso", eth->offload.tso);
+	if (eth->offload.ufo != NI_ETHERNET_SETTING_DEFAULT)
+		ni_dbus_dict_add_uint32(result, "ufo", eth->offload.ufo);
+	if (eth->offload.gso != NI_ETHERNET_SETTING_DEFAULT)
+		ni_dbus_dict_add_uint32(result, "gso", eth->offload.gso);
+	if (eth->offload.gro != NI_ETHERNET_SETTING_DEFAULT)
+		ni_dbus_dict_add_uint32(result, "gro", eth->offload.gro);
+	if (eth->offload.lro != NI_ETHERNET_SETTING_DEFAULT)
+		ni_dbus_dict_add_uint32(result, "lro", eth->offload.lro);
+
+	return TRUE;
+}
+
+static dbus_bool_t
+__ni_objectmodel_ethernet_set_offload(ni_dbus_object_t *object,
+				const ni_dbus_property_t *property,
+				const ni_dbus_variant_t *argument,
+				DBusError *error)
+{
+	ni_ethernet_t *eth;
+	
+	if (!(eth = ni_objectmodel_get_ethernet(object, error)))
+		return FALSE;
+
+	if (!ni_dbus_variant_is_dict(argument))
+		return FALSE;
+
+	if (!ni_dbus_dict_get_uint32(argument, "rx-csum", &eth->offload.rx_csum))
+		eth->offload.rx_csum = NI_ETHERNET_SETTING_DEFAULT;
+	if (!ni_dbus_dict_get_uint32(argument, "tx-csum", &eth->offload.tx_csum))
+		eth->offload.tx_csum = NI_ETHERNET_SETTING_DEFAULT;
+	if (!ni_dbus_dict_get_uint32(argument, "scatter-gather", &eth->offload.scatter_gather))
+		eth->offload.scatter_gather = NI_ETHERNET_SETTING_DEFAULT;
+	if (!ni_dbus_dict_get_uint32(argument, "tso", &eth->offload.tso))
+		eth->offload.tso = NI_ETHERNET_SETTING_DEFAULT;
+	if (!ni_dbus_dict_get_uint32(argument, "ufo", &eth->offload.ufo))
+		eth->offload.ufo = NI_ETHERNET_SETTING_DEFAULT;
+	if (!ni_dbus_dict_get_uint32(argument, "gso", &eth->offload.gso))
+		eth->offload.gso = NI_ETHERNET_SETTING_DEFAULT;
+	if (!ni_dbus_dict_get_uint32(argument, "gro", &eth->offload.gro))
+		eth->offload.gro = NI_ETHERNET_SETTING_DEFAULT;
+	if (!ni_dbus_dict_get_uint32(argument, "lro", &eth->offload.lro))
+		eth->offload.lro = NI_ETHERNET_SETTING_DEFAULT;
+
+	return TRUE;
+}
+
 
 
 #define ETHERNET_UINT_PROPERTY(dbus_name, member_name, rw) \
@@ -75,6 +141,9 @@ const ni_dbus_property_t	ni_objectmodel_ethernet_property_table[] = {
 	__NI_DBUS_PROPERTY(
 			DBUS_TYPE_ARRAY_AS_STRING DBUS_TYPE_BYTE_AS_STRING,
 			address, __ni_objectmodel_ethernet, RO),
+	__NI_DBUS_PROPERTY(
+			NI_DBUS_DICT_SIGNATURE,
+			offload, __ni_objectmodel_ethernet, RO),
 
 	{ NULL }
 };
