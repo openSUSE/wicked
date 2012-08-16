cfe1f36c112fb024acafd6f49443bd8e847d44d7
Olaf Kirch <okir@suse.de>
2012-08-16 11:30:06 +0200

    Get rid of obsolete ni_layer_t enum
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- include/wicked/constants.h
+++ include/wicked/constants.h
@@ -7,13 +7,6 @@
 #ifndef __WICKED_CONSTANTS_H__
 #define __WICKED_CONSTANTS_H__
 
-typedef enum ni_layer {
-	NI_LAYER_NONE = 0,
-	NI_LAYER_LINK,
-	NI_LAYER_LINK_AUTH,
-	NI_LAYER_ADDRCONF,
-} ni_layer_t;
-
 /*
  * Address configuration modes
  */
