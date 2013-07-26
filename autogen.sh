b4d1630b0f32eb8b09f62743439c17c4e59a6a2e
Marius Tomaschewski <mt@suse.de>
2013-07-26 12:17:58 +0200

    autogen: use same libexecdir as while rpmbuild

--- autogen.sh
+++ autogen.sh
@@ -8,14 +8,15 @@ pushd "${srcdir}" >/dev/null
 autoreconf --force --install     || exit 1
 popd >/dev/null
 
-case "$(uname -m)" in
+_lib=`rpm -E '%_lib' 2>/dev/null`
+test -n "$_lib" || case "$(uname -m)" in
 	x86_64|s390x|ppc64|powerpc64) _lib=lib64 ;;
 	*) _lib=lib ;;
 esac
 
 export CFLAGS="${CFLAGS:- -O1 -g -D_FORTIFY_SOURCE=2 -fstack-protector -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-parameter}"
 prefix=/usr
-defaults=(--sysconfdir=/etc --prefix=${prefix} --libdir=${prefix}/${_lib} --libexecdir=${prefix}/${_lib} --datadir=${prefix}/share --localstatedir=/var)
+defaults=(--sysconfdir=/etc --prefix=${prefix} --libdir=${prefix}/${_lib} --libexecdir=${prefix}/lib --datadir=${prefix}/share --localstatedir=/var)
 
 "${srcdir}/configure" "${@:-${defaults[@]}}"
 
