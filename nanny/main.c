92d947c1487fa893cbf2dda00c846efb3b848896
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2013-12-17 12:03:08 +0100

    nanny: Cleanup return codes
    Introducation of a new, common option: --systemd.
    This options is supposed to make sure no non-zero return codes
    are gave back. To be used with systemd's service files.
    
    Make use of new LSB return code enums.

--- nanny/main.c
+++ nanny/main.c
@@ -36,6 +36,7 @@ enum {
 	OPT_DEBUG,
 	OPT_LOG_LEVEL,
 	OPT_LOG_TARGET,
+	OPT_SYSTEMD,
 
 	OPT_FOREGROUND,
 	OPT_NOMODEMMGR,
@@ -49,6 +50,7 @@ static struct option	options[] = {
 	{ "debug",		required_argument,	NULL,	OPT_DEBUG },
 	{ "log-level",		required_argument,	NULL,	OPT_LOG_LEVEL },
 	{ "log-target",		required_argument,	NULL,	OPT_LOG_TARGET },
+	{ "systemd",		no_argument,		NULL,	OPT_SYSTEMD },
 
 	/* daemon */
 	{ "foreground",		no_argument,		NULL,	OPT_FOREGROUND },
@@ -61,8 +63,9 @@ static struct option	options[] = {
 
 static const char *	program_name;
 static const char *	opt_log_target;
-static int		opt_foreground;
-static int		opt_no_modem_manager;
+static ni_bool_t	opt_foreground;
+static ni_bool_t	opt_no_modem_manager;
+static ni_bool_t	opt_systemd;
 
 static void		babysit(void);
 static void		ni_nanny_discover_state(ni_nanny_t *);
@@ -103,17 +106,19 @@ main(int argc, char **argv)
 				"        Run as a foreground process, rather than as a daemon.\n"
 				"  --log-target target\n"
 				"        Set log destination target to <target>.\n"
+				"  --systemd\n"
+				"        Enables behavior required by wicked.service under systemd\n"
 				, program_name);
-			return (c == OPT_HELP ? 0 : 1);
+			return (c == OPT_HELP ? NI_LSB_RC_SUCCESS : NI_LSB_RC_USAGE);
 
 		case OPT_VERSION:
 			printf("%s %s\n", program_name, PACKAGE_VERSION);
-			return 0;
+			return NI_LSB_RC_SUCCESS;
 
 		case OPT_CONFIGFILE:
 			if (!ni_set_global_config_path(optarg)) {
 				fprintf(stderr, "Unable to set config file '%s': %m\n", optarg);
-				return 1;
+				return NI_LSB_RC_ERROR;
 			}
 			break;
 
@@ -121,18 +126,18 @@ main(int argc, char **argv)
 			if (!strcmp(optarg, "help")) {
 				printf("Supported debug facilities:\n");
 				ni_debug_help();
-				return 0;
+				return NI_LSB_RC_SUCCESS;
 			}
 			if (ni_enable_debug(optarg) < 0) {
 				fprintf(stderr, "Bad debug facility \"%s\"\n", optarg);
-				return 1;
+				goto usage;
 			}
 			break;
 
 		case OPT_LOG_LEVEL:
 			if (!ni_log_level_set(optarg)) {
 				fprintf(stderr, "Bad log level \%s\"\n", optarg);
-				return 1;
+				goto usage;
 			}
 			break;
 
@@ -141,11 +146,15 @@ main(int argc, char **argv)
 			break;
 
 		case OPT_FOREGROUND:
-			opt_foreground = 1;
+			opt_foreground = TRUE;
 			break;
 
 		case OPT_NOMODEMMGR:
-			opt_no_modem_manager = 1;
+			opt_no_modem_manager = TRUE;
+			break;
+
+		case OPT_SYSTEMD:
+			opt_systemd = TRUE;
 			break;
 		}
 	}
@@ -157,16 +166,18 @@ main(int argc, char **argv)
 		if (!ni_log_destination(program_name, opt_log_target)) {
 			fprintf(stderr, "Bad log destination \%s\"\n",
 				opt_log_target);
+			goto usage;
 		}
-		return 1;
-	} else if (opt_foreground && getppid() != 1) {
-		ni_log_destination(program_name, "syslog::perror");
-	} else {
+	}
+	else if (opt_systemd || getppid() == 1 || !opt_foreground) { /* syslog only */
 		ni_log_destination(program_name, "syslog");
+	}
+	else { /* syslog + stderr */
+		ni_log_destination(program_name, "syslog::perror");
 	}
 
 	babysit();
-	return 0;
+	return NI_LSB_RC_SUCCESS;
 }
 
 /*
