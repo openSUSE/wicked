64e4a0343dab7b96cf588c591c6dd880f4d298ca
Marius Tomaschewski <mt@suse.de>
2014-01-28 08:25:53 +0100

    dhcp6: moved status and option names to src/dhcp6

--- dhcp6/protocol.h
+++ dhcp6/protocol.h
@@ -99,118 +99,6 @@ enum NI_DHCP6_MSG_TYPE {
 };
 
 /*
- * DHCPv6 Option Codes
- *
- * http://tools.ietf.org/html/rfc3315#section-24.3
- *
- * [Last Updated: 2012-03-29]
- * http://www.iana.org/assignments/dhcpv6-parameters/dhcpv6-parameters.xml
- */
-enum NI_DHCP6_OPTION {
-	/*					   0:	  Reserved   */
-	NI_DHCP6_OPTION_CLIENTID		=  1,	/* [RFC3315] */
-	NI_DHCP6_OPTION_SERVERID		=  2,
-	NI_DHCP6_OPTION_IA_NA			=  3,
-	NI_DHCP6_OPTION_IA_TA			=  4,
-	NI_DHCP6_OPTION_IAADDR			=  5,
-	NI_DHCP6_OPTION_ORO			=  6,
-	NI_DHCP6_OPTION_PREFERENCE		=  7,
-	NI_DHCP6_OPTION_ELAPSED_TIME		=  8,
-	NI_DHCP6_OPTION_RELAY_MSG		=  9,
-	/*					  10:	  Unassigned */
-	NI_DHCP6_OPTION_AUTH			= 11,
-	NI_DHCP6_OPTION_UNICAST			= 12,
-	NI_DHCP6_OPTION_STATUS_CODE		= 13,
-	NI_DHCP6_OPTION_RAPID_COMMIT		= 14,
-	NI_DHCP6_OPTION_USER_CLASS		= 15,
-	NI_DHCP6_OPTION_VENDOR_CLASS		= 16,
-	NI_DHCP6_OPTION_VENDOR_OPTS		= 17,
-	NI_DHCP6_OPTION_INTERFACE_ID		= 18,
-	NI_DHCP6_OPTION_RECONF_MSG		= 19,
-	NI_DHCP6_OPTION_RECONF_ACCEPT		= 20,
-	NI_DHCP6_OPTION_SIP_SERVER_D		= 21,	/* [RFC3319] */
-	NI_DHCP6_OPTION_SIP_SERVER_A		= 22,
-	NI_DHCP6_OPTION_DNS_SERVERS		= 23,	/* [RFC3646] */
-	NI_DHCP6_OPTION_DNS_DOMAINS		= 24,
-	NI_DHCP6_OPTION_IA_PD			= 25,	/* [RFC3633] */
-	NI_DHCP6_OPTION_IA_PREFIX		= 26,
-	NI_DHCP6_OPTION_NIS_SERVERS		= 27,	/* [RFC3898] */
-	NI_DHCP6_OPTION_NISP_SERVERS		= 28,
-	NI_DHCP6_OPTION_NIS_DOMAIN_NAME		= 29,
-	NI_DHCP6_OPTION_NISP_DOMAIN_NAME	= 30,
-	NI_DHCP6_OPTION_SNTP_SERVERS		= 31,	/* [RFC4075] */
-	NI_DHCP6_OPTION_INFO_REFRESH_TIME	= 32,	/* [RFC4242] */
-	NI_DHCP6_OPTION_BCMCS_SERVER_D		= 33,	/* [RFC4280] */
-	NI_DHCP6_OPTION_BCMCS_SERVER_A		= 34,
-	/*					  35:	  Unassigned */
-	NI_DHCP6_OPTION_GEOCONF_CIVIC		= 36,	/* [RFC4776] */
-	NI_DHCP6_OPTION_REMOTE_ID		= 37,	/* [RFC4649] */
-	NI_DHCP6_OPTION_SUBSCRIBER_ID		= 38,	/* [RFC4580] */
-	NI_DHCP6_OPTION_FQDN			= 39,	/* [RFC4704] */
-	NI_DHCP6_OPTION_PANA_AGENT		= 40,	/* [RFC5192] */
-	NI_DHCP6_OPTION_POSIX_TIMEZONE		= 41,	/* [RFC4833] */
-	NI_DHCP6_OPTION_POSIX_TIMEZONEDB	= 42,	/* [RFC4833] */
-	NI_DHCP6_OPTION_ERO			= 43,	/* [RFC4994] */
-	NI_DHCP6_OPTION_LQ_QUERY		= 44,	/* [RFC5007] */
-	NI_DHCP6_OPTION_CLIENT_DATA		= 45,
-	NI_DHCP6_OPTION_CLT_TIME		= 46,
-	NI_DHCP6_OPTION_LQ_RELAY_DATA		= 47,
-	NI_DHCP6_OPTION_LQ_CLIENT_LINK		= 48,
-	NI_DHCP6_OPTION_MIP6_HNINF		= 49,	/* [RFC-ietf-mip6-hiopt-17] */
-	NI_DHCP6_OPTION_MIP6_RELAY		= 50,	/* [RFC-ietf-mip6-hiopt-17] */
-	NI_DHCP6_OPTION_V6_LOST			= 51,	/* [RFC5223] */
-	NI_DHCP6_OPTION_CAPWAP_AC_V6		= 52,	/* [RFC5417] */
-	NI_DHCP6_OPTION_RELAY_ID		= 53,	/* [RFC5460] */
-	NI_DHCP6_OPTION_MOS_ADDRESSES		= 54,	/* [RFC5678] */
-	NI_DHCP6_OPTION_MOS_DOMAINS		= 55,
-	NI_DHCP6_OPTION_NTP_SERVER		= 56,	/* [RFC5908] */
-	NI_DHCP6_OPTION_V6_ACCESS_DOMAIN	= 57,	/* [RFC5986] */
-	NI_DHCP6_OPTION_SIP_UA_CS_LIST		= 58,	/* [RFC6011] */
-	NI_DHCP6_OPTION_BOOTFILE_URL		= 59,	/* [RFC5970] */
-	NI_DHCP6_OPTION_BOOTFILE_PARAM		= 60,	/* [RFC5970] */
-	NI_DHCP6_OPTION_CLIENT_ARCH_TYPE	= 61,	/* [RFC5970] */
-	NI_DHCP6_OPTION_NII			= 62,	/* [RFC5970] */
-	NI_DHCP6_OPTION_GEOLOCATION		= 63,	/* [RFC6225] */
-	NI_DHCP6_OPTION_AFTR_NAME		= 64,	/* [RFC6334] */
-	NI_DHCP6_OPTION_ERP_LOCAL_DOMAIN	= 65,	/* [RFC6440] */
-	NI_DHCP6_OPTION_RSOO			= 66,	/* [RFC6422] */
-	NI_DHCP6_OPTION_PD_EXCLUDE		= 67,	/* [RFC-ietf-dhc-pd-exclude-04] */
-	NI_DHCP6_OPTION_VSS			= 68,	/* [RFC-ietf-dhc-vpn-option-15] */
-	/*					69-255:	  Unassigned */
-
-	__NI_DHCP6_OPTION_MAX
-};
-
-/*
- * DHCPv6 Status Codes
- * http://tools.ietf.org/html/rfc3315#section-24.4
- *
- * Success         0 Success.
- * UnspecFail      1 Failure, reason unspecified; this
- *                   status code is sent by either a client
- *                   or a server to indicate a failure not
- *                   explicitly specified in this document.
- * NoAddrsAvail    2 Server has no addresses available to
- *                   assign to the IA(s).
- * NoBinding       3 Client record (binding) unavailable.
- * NotOnLink       4 The prefix for the address is not appropriate
- *                   for the link to which the client is attached.
- * UseMulticast    5 Sent by a server to a client to force the
- *                   client to send messages to the server using
- *                   the All_DHCP_Relay_Agents_and_Servers address.
- */
-enum NI_DHCP6_STATUS_CODE {
-	NI_DHCP6_STATUS_SUCCESS			= 0,
-	NI_DHCP6_STATUS_FAILURE			= 1,
-	NI_DHCP6_STATUS_NOADDRS			= 2,
-	NI_DHCP6_STATUS_NOBINDING		= 3,
-	NI_DHCP6_STATUS_NOTONLINK		= 4,
-	NI_DHCP6_STATUS_USEMULTICAST		= 5,
-
-	__NI_DHCP6_STATUS_MAX
-};
-
-/*
  * DHCPv6 Timings
  *
  * http://tools.ietf.org/html/rfc3315#section-5.5
@@ -312,9 +200,6 @@ typedef struct ni_dhcp6_option_request {
 /*
  * functions used in device.c and fsm.c
  */
-extern const char *	ni_dhcp6_option_name(unsigned int);
-const char *		ni_dhcp6_status_name(unsigned int);
-
 extern const char *	ni_dhcp6_message_name(unsigned int);
 extern unsigned int	ni_dhcp6_message_xid(unsigned int);
 
