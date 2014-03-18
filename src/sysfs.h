94482214eeeded9c07c758f38049c2da6d81ce1b
Marius Tomaschewski <mt@suse.de>
2014-03-18 19:42:14 +0100

    sysfs: added ipv6_ifconfig_get/set int variants

--- src/sysfs.h
+++ src/sysfs.h
@@ -47,7 +47,9 @@ extern void	ni_sysfs_bridge_port_get_status(const char *, ni_bridge_port_status_
 extern ni_pci_dev_t *ni_sysfs_netdev_get_pci(const char *ifname);
 
 extern int	ni_sysctl_ipv6_ifconfig_is_present(const char *ifname);
+extern int	ni_sysctl_ipv6_ifconfig_get_int(const char *, const char *, int *);
 extern int	ni_sysctl_ipv6_ifconfig_get_uint(const char *, const char *, unsigned int *);
+extern int	ni_sysctl_ipv6_ifconfig_set_int(const char *, const char *, int);
 extern int	ni_sysctl_ipv6_ifconfig_set_uint(const char *, const char *, unsigned int);
 
 extern int	ni_sysctl_ipv4_ifconfig_is_present(const char *ifname);
