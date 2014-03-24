e34761a0c9c0617035902257ebcaa33defb4c6a7
Karol Mroz <kmroz@suse.com>
2014-03-24 13:53:06 -0700

    dbus: enable macvtap service

--- src/dbus-objects/interface.c
+++ src/dbus-objects/interface.c
@@ -104,6 +104,7 @@ ni_objectmodel_register_netif_services(void)
 	ni_objectmodel_register_netif_service(NI_IFTYPE_ETHERNET, &ni_objectmodel_ethernet_service);
 	ni_objectmodel_register_netif_service(NI_IFTYPE_VLAN, &ni_objectmodel_vlan_service);
 	ni_objectmodel_register_netif_service(NI_IFTYPE_MACVLAN, &ni_objectmodel_macvlan_service);
+	ni_objectmodel_register_netif_service(NI_IFTYPE_MACVTAP, &ni_objectmodel_macvtap_service);
 	ni_objectmodel_register_netif_service(NI_IFTYPE_DUMMY, &ni_objectmodel_dummy_service);
 	ni_objectmodel_register_netif_service(NI_IFTYPE_BOND, &ni_objectmodel_bond_service);
 	ni_objectmodel_register_netif_service(NI_IFTYPE_BRIDGE, &ni_objectmodel_bridge_service);
@@ -117,6 +118,7 @@ ni_objectmodel_register_netif_services(void)
 	ni_objectmodel_register_netif_factory_service(&ni_objectmodel_bridge_factory_service);
 	ni_objectmodel_register_netif_factory_service(&ni_objectmodel_vlan_factory_service);
 	ni_objectmodel_register_netif_factory_service(&ni_objectmodel_macvlan_factory_service);
+	ni_objectmodel_register_netif_factory_service(&ni_objectmodel_macvtap_factory_service);
 	ni_objectmodel_register_netif_factory_service(&ni_objectmodel_dummy_factory_service);
 	ni_objectmodel_register_netif_factory_service(&ni_objectmodel_tun_factory_service);
 	ni_objectmodel_register_netif_factory_service(&ni_objectmodel_tap_factory_service);
