67051c738eb9f9f6d0c95cf4801fb89b78957596
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2014-05-09 16:20:37 +0200

    client: Add defines for configuration strings

---
+++ src/client/ifconfig.h
@@ -0,0 +1,43 @@
+/*
+ *	wicked client ifconfig structures and objects
+ *
+ *	Copyright (C) 2010-2014 SUSE LINUX Products GmbH, Nuernberg, Germany.
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this program; if not, see <http://www.gnu.org/licenses/> or write
+ *	to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *	Boston, MA 02110-1301 USA.
+ *
+ *	Authors:
+ *		Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
+ *
+ */
+#ifndef   __WICKED_CLIENT_IFCONFIG_H__
+#define   __WICKED_CLIENT_IFCONFIG_H__
+
+#define NI_CLIENT_IFCONFIG		"interface"
+#define NI_CLIENT_IFCONFIG_MATCH_NAME	"name"
+
+#define NI_NANNY_IFPOLICY		"policy"
+#define NI_NANNY_IFTEMPLATE		"template"
+
+#define NI_NANNY_IFPOLICY_MATCH		"match"
+#define NI_NANNY_IFPOLICY_MATCH_DEV	"device"
+#define NI_NANNY_IFPOLICY_MERGE		"merge"
+
+#define NI_NANNY_IFPOLICY_NAME		"name"
+#define NI_NANNY_IFPOLICY_ORIGIN	"origin"
+#define NI_NANNY_IFPOLICY_UUID		"uuid"
+#define NI_NANNY_IFPOLICY_OWNER		"owner"
+
+#endif /* __WICKED_CLIENT_IFCONFIG_H__ */
