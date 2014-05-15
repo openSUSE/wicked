9d7d40f6fd661e9a32c02de7c0e749cc60cd2893
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2014-05-15 16:42:15 +0200

    fsm: export ni_ifworker_fail() and ni_ifworker_success()

--- include/wicked/fsm.h
+++ include/wicked/fsm.h
@@ -290,6 +290,8 @@ extern ni_bool_t		ni_ifworker_check_config(const ni_ifworker_t *, const xml_node
 extern void			ni_ifworker_reset(ni_ifworker_t *);
 extern int			ni_ifworker_bind_early(ni_ifworker_t *, ni_fsm_t *, ni_bool_t);
 extern int			ni_ifworker_start(ni_fsm_t *, ni_ifworker_t *, unsigned long);
+extern void			ni_ifworker_fail(ni_ifworker_t *, const char *, ...);
+extern void			ni_ifworker_success(ni_ifworker_t *);
 extern void			ni_ifworker_set_completion_callback(ni_ifworker_t *, void (*)(ni_ifworker_t *), void *);
 extern ni_rfkill_type_t		ni_ifworker_get_rfkill_type(const ni_ifworker_t *);
 extern void			ni_ifworker_free(ni_ifworker_t *);
