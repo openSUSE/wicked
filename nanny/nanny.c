e680c02a482a8ff1870b404676a741f1403e673d
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2014-05-23 10:57:21 +0200

    nanny: Factory device handling by nanny
    - Perform ni_fsm_build_hierarchy() only upon
      new ifworker arrival
    - Do not destroy recheck array each time
    - Clean recheck upon device delete event
    - Schedule non-existing factory devices

--- nanny/nanny.c
+++ nanny/nanny.c
@@ -181,8 +181,7 @@ ni_nanny_recheck_do(ni_nanny_t *mgr)
 	if (mgr->recheck.count == 0)
 		return;
 
-	ni_fsm_refresh_state(mgr->fsm);
-	ni_fsm_build_hierarchy(fsm);
+	ni_fsm_refresh_state(fsm);
 
 	for (i = 0; i < mgr->recheck.count; ++i) {
 		ni_ifworker_t *w = mgr->recheck.data[i];
@@ -191,8 +190,6 @@ ni_nanny_recheck_do(ni_nanny_t *mgr)
 			ni_nanny_recheck(mgr, w);
 
 	}
-
-	ni_ifworker_array_destroy(&mgr->recheck);
 }
 
 /*
@@ -404,6 +401,7 @@ ni_nanny_unregister_device(ni_nanny_t *mgr, ni_ifworker_t *w)
 	ni_nanny_remove_device(mgr, mdev);
 	ni_objectmodel_unregister_managed_device(mdev);
 	ni_fsm_destroy_worker(mgr->fsm, w);
+	ni_ifworker_array_remove(&mgr->recheck, w);
 }
 
 /*
@@ -650,6 +648,7 @@ ni_nanny_netif_state_change_signal_receive(ni_dbus_connection_t *conn, ni_dbus_m
 		// a VLAN or vif, or a hotplug device
 		// Create a worker and a managed_netif for this device.
 		if ((w = ni_fsm_recv_new_netif_path(mgr->fsm, object_path))) {
+			ni_fsm_build_hierarchy(mgr->fsm);
 			ni_nanny_register_device(mgr, w);
 			ni_nanny_schedule_recheck(mgr, w);
 		}
@@ -814,6 +813,20 @@ ni_objectmodel_nanny_get_device(ni_dbus_object_t *object, const ni_dbus_method_t
 	return TRUE;
 }
 
+static void
+__ni_objectmodel_nanny_factory_device_recheck(ni_nanny_t *mgr, const char *ifname)
+{
+	ni_ifworker_t *w;
+
+	if (!mgr || ni_string_empty(ifname))
+		return;
+
+	if (!(w = ni_fsm_ifworker_by_name(mgr->fsm, NI_IFWORKER_TYPE_NETDEV, ifname)))
+		return;
+
+	if (!ni_ifworker_complete(w) && ni_ifworker_is_factory_device(w))
+		ni_nanny_schedule_recheck(mgr, w);
+}
 
 /*
  * Nanny.createPolicy()
@@ -863,7 +876,17 @@ ni_objectmodel_nanny_create_policy(ni_dbus_object_t *object, const ni_dbus_metho
 			return FALSE;
 		}
 
+		/* Create policy and corresponding worker (e.g. for hotplug or factory devices */
 		policy = ni_fsm_policy_new(mgr->fsm, pname, pnode);
+
+		/* Rebuild the hierarchy cause new policy may hit some matches */
+		ni_fsm_build_hierarchy(mgr->fsm);
+
+		/* Schedule recheck on Factory devices
+		 * (Hotplugs and existing devices are scheduled upon DEVICE_READY)
+		 */
+		__ni_objectmodel_nanny_factory_device_recheck(mgr, pname);
+
 		policy_object = ni_objectmodel_register_managed_policy(ni_dbus_object_get_server(object),
 			ni_managed_policy_new(mgr, policy, NULL));
 
