f4bdc98e04efc257f60bdf674ae5e893e5392083
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2014-07-09 13:11:52 +0200

    nanny: destroy worker after unschedule when unregister

--- nanny/nanny.c
+++ nanny/nanny.c
@@ -450,8 +450,8 @@ ni_nanny_unregister_device(ni_nanny_t *mgr, ni_ifworker_t *w)
 
 	ni_nanny_remove_device(mgr, mdev);
 	ni_objectmodel_unregister_managed_device(mdev);
-	ni_fsm_destroy_worker(mgr->fsm, w);
 	ni_nanny_unschedule(&mgr->recheck, w);
+	ni_fsm_destroy_worker(mgr->fsm, w);
 }
 
 /*
