c626d280fd21de684233b990f3a2e820008c3e88
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2014-07-15 11:43:25 +0200

    ifreload: reset also pulled in children

--- client/ifreload.c
+++ client/ifreload.c
@@ -293,6 +293,7 @@ usage:
 		ni_debug_application("No interfaces to be brought down\n");
 	}
 
+	ni_fsm_pull_in_children(&up_marked);
 	/* Drop deleted or apply the up range */
 	ni_fsm_reset_matching_workers(fsm, &up_marked, &up_range, FALSE);
 
@@ -300,7 +301,6 @@ usage:
 	if (up_marked.count) {
 		/* And trigger up */
 		ni_debug_application("Reloading all changed devices");
-		ni_fsm_pull_in_children(&up_marked);
 		if (ni_fsm_start_matching_workers(fsm, &up_marked)) {
 			/* Execute the up run */
 			if (ni_fsm_schedule(fsm) != 0)
