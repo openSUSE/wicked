ecd695224dd5c65c11fbffffb91ff0c12c96549a
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2015-03-09 16:14:05 +0100

    fsm: Update client_state only on DEVICE_READY event
    Update only at DEVICE_READY should be enough _and_ this
    prevents from unwanted release of pending dbus events,
    causing potential race conditions in fsm transitions and
    events handling.

--- src/fsm.c
+++ src/fsm.c
@@ -1417,8 +1417,7 @@ ni_ifworker_set_state(ni_ifworker_t *w, unsigned int new_state)
 		if (w->fsm.wait_for && w->fsm.wait_for->next_state == new_state)
 			w->fsm.wait_for = NULL;
 
-		if ((new_state == NI_FSM_STATE_DEVICE_READY ||
-		    new_state == NI_FSM_STATE_DEVICE_SETUP) && w->object && !w->readonly) {
+		if ((new_state == NI_FSM_STATE_DEVICE_READY) && w->object && !w->readonly) {
 			ni_ifworker_update_client_state_control(w);
 			ni_ifworker_update_client_state_config(w);
 		}
