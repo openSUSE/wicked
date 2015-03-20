fddd7aac795a1404a8fa2620736177f38f81c522
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2015-03-20 15:26:53 +0100

    fsm: do not look by ifindex in ni_fsm_ifworker_by_netdev()

--- src/fsm.c
+++ src/fsm.c
@@ -838,8 +838,6 @@ ni_fsm_ifworker_by_netdev(ni_fsm_t *fsm, const ni_netdev_t *dev)
 
 		if (w->device == dev)
 			return w;
-		if (w->ifindex && w->ifindex == dev->link.ifindex)
-			return w;
 	}
 
 	return NULL;
