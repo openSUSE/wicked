83acc5614f8419f521052591125796afecfa87c9
Marius Tomaschewski <mt@suse.de>
2015-04-23 17:09:18 +0200

    ipv6: utility to rebase ipv6 address lifetime

--- include/wicked/address.h
+++ include/wicked/address.h
@@ -102,4 +102,7 @@ extern void		ni_address_list_dedup(ni_address_t **);
 extern ni_address_t *	ni_address_list_find(ni_address_t *, const ni_sockaddr_t *);
 extern unsigned int	ni_address_list_count(ni_address_t *list);
 
+extern void		ni_ipv6_cache_info_rebase(ni_ipv6_cache_info_t *, const ni_ipv6_cache_info_t *,
+					const struct timeval *);
+
 #endif /* __WICKED_ADDRESS_H__ */
