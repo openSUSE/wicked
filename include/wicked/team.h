3e655cff6b47b62e8040778d1c7dfb3e1417e8d4
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2015-08-13 19:48:09 +0200

    team: added link watch structs and schema

--- include/wicked/team.h
+++ include/wicked/team.h
@@ -25,6 +25,9 @@
 #ifndef NI_WICKED_TEAM_H
 #define NI_WICKED_TEAM_H
 
+/*
+ * runner
+ */
 typedef enum {
 	NI_TEAM_RUNNER_ROUND_ROBIN = 0,
 	NI_TEAM_RUNNER_ACTIVE_BACKUP,
@@ -34,18 +37,101 @@ typedef enum {
 	NI_TEAM_RUNNER_LACP,
 } ni_team_runner_type_t;
 
+
+/*
+ * runner type union
+ */
 typedef struct ni_team_runner {
 	ni_team_runner_type_t			type;
 } ni_team_runner_t;
 
+
+/*
+ * link watch
+ */
+typedef enum {
+	NI_TEAM_LINK_WATCH_ETHTOOL = 0,
+	NI_TEAM_LINK_WATCH_ARP_PING,
+	NI_TEAM_LINK_WATCH_NSNA_PING,
+	NI_TEAM_LINK_WATCH_TIPC,
+} ni_team_link_watch_type_t;
+
+/*
+ * link watch type structs
+ */
+typedef struct ni_team_link_watch_arp {
+	char *					source_host;
+	char *					target_host;
+
+	unsigned int				interval;
+	unsigned int				init_wait;
+	ni_bool_t				validate_active;
+	ni_bool_t				validate_inactive;
+	ni_bool_t				send_always;
+	unsigned int				missed_max;
+	unsigned int				missed;
+} ni_team_link_watch_arp_t;
+
+typedef struct ni_team_link_watch_tipc {
+	char *					bearer;
+} ni_team_link_watch_tipc_t;
+
+typedef struct ni_team_link_watch_nsna {
+	char *					target_host;
+
+	unsigned int				interval;
+	unsigned int				init_wait;
+	unsigned int				missed_max;
+	unsigned int				missed;
+} ni_team_link_watch_nsna_t;
+
+typedef struct ni_team_link_watch_ethtool {
+	unsigned int				delay_up;
+	unsigned int				delay_down;
+} ni_team_link_watch_ethtool_t;
+
+
+/*
+ * link watch type union
+ */
+typedef struct ni_team_link_watch {
+	ni_team_link_watch_type_t		type;
+
+	union {
+		ni_team_link_watch_arp_t	arp;
+		ni_team_link_watch_tipc_t	tipc;
+		ni_team_link_watch_nsna_t	nsna;
+		ni_team_link_watch_ethtool_t	ethtool; /* default */
+	};
+} ni_team_link_watch_t;
+
+typedef struct ni_team_link_watch_array {
+	unsigned int				count;
+	ni_team_link_watch_t **			data;
+} ni_team_link_watch_array_t;
+
+
+/*
+ * team device
+ */
 struct ni_team {
 	ni_team_runner_t			runner;
+	ni_team_link_watch_array_t		link_watch;
 };
+
 
 extern ni_team_t *				ni_team_new();
 extern void					ni_team_free(ni_team_t *);
 
 extern const char *				ni_team_runner_type_to_name(ni_team_runner_type_t);
 extern ni_bool_t				ni_team_runner_name_to_type(const char *, ni_team_runner_type_t *);
+
+extern const char *				ni_team_link_watch_type_to_name(ni_team_link_watch_type_t);
+extern ni_bool_t				ni_team_link_watch_name_to_type(const char *, ni_team_link_watch_type_t *);
+
+extern ni_team_link_watch_t *			ni_team_link_watch_new(ni_team_link_watch_type_t);
+extern void					ni_team_link_watch_free(ni_team_link_watch_t *);
+extern ni_bool_t				ni_team_link_watch_array_append(ni_team_link_watch_array_t *, ni_team_link_watch_t *);
+extern ni_bool_t				ni_team_link_watch_array_delete_at(ni_team_link_watch_array_t *, unsigned int);
 
 #endif /* NI_WICKED_TEAM_H */
--- src/team.c
+++ src/team.c
@@ -32,6 +32,8 @@
 #include <wicked/team.h>
 #include "util_priv.h"
 
+#define NI_TEAM_LINK_WATCH_ARRAY_CHUNK		4
+
 
 /*
  * Map teamd mode names to constants
@@ -68,6 +70,130 @@ ni_team_runner_name_to_type(const char *name, ni_team_runner_type_t *type)
 	return TRUE;
 }
 
+
+/*
+ * Map teamd link watch names to constants
+ */
+static const ni_intmap_t	ni_team_link_watch_names[] = {
+	{ "ethtool",		NI_TEAM_LINK_WATCH_ETHTOOL	},
+	{ "arp_ping",		NI_TEAM_LINK_WATCH_ARP_PING	},
+	{ "nsna_ping",		NI_TEAM_LINK_WATCH_NSNA_PING	},
+	{ "tipc",		NI_TEAM_LINK_WATCH_TIPC		},
+
+	{ NULL,			-1U				}
+};
+
+const char *
+ni_team_link_watch_type_to_name(ni_team_link_watch_type_t type)
+{
+	return ni_format_uint_mapped(type, ni_team_link_watch_names);
+}
+
+ni_bool_t
+ni_team_link_watch_name_to_type(const char *name, ni_team_link_watch_type_t *type)
+{
+	unsigned int _type;
+
+	if (!name || !type)
+		return FALSE;
+
+	if (ni_parse_uint_mapped(name, ni_team_link_watch_names, &_type) != 0)
+		return FALSE;
+
+	*type = _type;
+	return TRUE;
+}
+
+ni_team_link_watch_t *
+ni_team_link_watch_new(ni_team_link_watch_type_t type)
+{
+	ni_team_link_watch_t *lw;
+
+	lw = xcalloc(1, sizeof(*lw));
+	lw->type = type;
+
+	return lw;
+}
+
+void
+ni_team_link_watch_free(ni_team_link_watch_t *lw)
+{
+	if (lw) switch (lw->type) {
+	case NI_TEAM_LINK_WATCH_ETHTOOL:
+		break;
+	case NI_TEAM_LINK_WATCH_ARP_PING:
+		ni_string_free(&lw->arp.source_host);
+		ni_string_free(&lw->arp.target_host);
+		break;
+	case NI_TEAM_LINK_WATCH_NSNA_PING:
+		ni_string_free(&lw->nsna.target_host);
+		break;
+	case NI_TEAM_LINK_WATCH_TIPC:
+		ni_string_free(&lw->tipc.bearer);
+		break;
+	default:
+		return;
+	}
+	free(lw);
+}
+
+static inline void
+ni_team_link_watch_array_init(ni_team_link_watch_array_t *array)
+{
+	memset(array, 0, sizeof(*array));
+}
+
+void
+ni_team_link_watch_array_destroy(ni_team_link_watch_array_t *array)
+{
+	while (array->count > 0)
+		ni_team_link_watch_free(array->data[--array->count]);
+	free(array->data);
+	ni_team_link_watch_array_init(array);
+}
+
+static void
+__ni_team_link_watch_array_realloc(ni_team_link_watch_array_t *array, unsigned int newsize)
+{
+	ni_team_link_watch_t **newdata;
+	unsigned int i;
+
+	newsize = (newsize + NI_TEAM_LINK_WATCH_ARRAY_CHUNK);
+	newdata = xrealloc(array->data, newsize * sizeof(ni_team_link_watch_t));
+	array->data = newdata;
+	for (i = array->count; i < newsize; ++i)
+		array->data[i] = NULL;
+}
+
+ni_bool_t
+ni_team_link_watch_array_append(ni_team_link_watch_array_t *array, ni_team_link_watch_t *lw)
+{
+	if (array && lw) {
+		if ((array->count % NI_TEAM_LINK_WATCH_ARRAY_CHUNK) == 0)
+			__ni_team_link_watch_array_realloc(array, array->count);
+
+		array->data[array->count++] = lw;
+		return TRUE;
+	}
+	return FALSE;
+}
+
+ni_bool_t
+ni_team_link_watch_array_delete_at(ni_team_link_watch_array_t *array, unsigned int pos)
+{
+	if (!array || pos >= array->count)
+		return FALSE;
+
+	ni_team_link_watch_free(array->data[pos]);
+	array->count--;
+	if (pos < array->count) {
+		memmove(&array->data[pos], &array->data[pos + 1],
+			(array->count - pos) * sizeof(ni_team_link_watch_t *));
+	}
+	array->data[array->count] = NULL;
+	return TRUE;
+}
+
 ni_team_t *
 ni_team_new(void)
 {
@@ -81,6 +207,7 @@ void
 ni_team_free(ni_team_t *team)
 {
 	if (team) {
+		ni_team_link_watch_array_destroy(&team->link_watch);
 		free(team);
 	}
 }
