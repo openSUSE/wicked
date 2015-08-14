4317f95ab01757fecb6a0b87e6d5b7b918f8615a
Pawel Wieczorkiewicz <pwieczorkiewicz@suse.de>
2015-08-14 14:21:10 +0200

    team: added link_watch dbus objectmodel properties

--- src/dbus-objects/team.c
+++ src/dbus-objects/team.c
@@ -474,10 +474,220 @@ __ni_objectmodel_team_set_runner(ni_dbus_object_t *object, const ni_dbus_propert
 	return TRUE;
 }
 
-#define TEAM_INT_PROPERTY(dbus_name, member_name, rw) \
-	NI_DBUS_GENERIC_INT_PROPERTY(team, dbus_name, member_name, rw)
-#define TEAM_UINT_PROPERTY(dbus_name, member_name, rw) \
-	NI_DBUS_GENERIC_UINT_PROPERTY(team, dbus_name, member_name, rw)
+/*
+ * Helper functions to represent link_watches as a dbus dict
+ */
+static dbus_bool_t
+__ni_objectmodel_team_link_watch_to_dict(const ni_team_link_watch_t *lw, ni_dbus_variant_t *dict,
+				DBusError *error, const ni_dbus_object_t *object, const ni_dbus_property_t *property)
+{
+	if (!lw || !dict)
+		return FALSE;
+
+	switch(lw->type) {
+	case NI_TEAM_LINK_WATCH_ETHTOOL:
+		ni_dbus_dict_add_uint32(dict, "down_up", lw->ethtool.delay_up);
+		ni_dbus_dict_add_uint32(dict, "down_down", lw->ethtool.delay_down);
+		break;
+	case NI_TEAM_LINK_WATCH_ARP_PING:
+		if (lw->arp.source_host)
+			ni_dbus_dict_add_string(dict, "source_host", lw->arp.source_host);
+		if (lw->arp.target_host)
+			ni_dbus_dict_add_string(dict, "target_host", lw->arp.target_host);
+		if (lw->arp.interval)
+			ni_dbus_dict_add_uint32(dict, "interval", lw->arp.interval);
+		if (lw->arp.init_wait)
+			ni_dbus_dict_add_uint32(dict, "init_wait", lw->arp.init_wait);
+		if (lw->arp.validate_active)
+			ni_dbus_dict_add_bool(dict, "validate_active", lw->arp.validate_active);
+		if (lw->arp.validate_inactive)
+			ni_dbus_dict_add_bool(dict, "validate_inactive", lw->arp.validate_inactive);
+		if (lw->arp.send_always)
+			ni_dbus_dict_add_bool(dict, "send_always", lw->arp.send_always);
+		if (lw->arp.missed_max > 0)
+			ni_dbus_dict_add_uint32(dict, "missed_max", lw->arp.missed_max);
+		if (lw->arp.missed > 0)
+			ni_dbus_dict_add_uint32(dict, "missed", lw->arp.missed);
+		break;
+	case NI_TEAM_LINK_WATCH_NSNA_PING:
+		if (lw->nsna.target_host)
+			ni_dbus_dict_add_string(dict, "target_host", lw->nsna.target_host);
+		if (lw->nsna.interval > 0)
+			ni_dbus_dict_add_uint32(dict, "interval", lw->nsna.interval);
+		if (lw->nsna.init_wait > 0)
+			ni_dbus_dict_add_uint32(dict, "init_wait", lw->nsna.init_wait);
+		if (lw->nsna.missed_max)
+			ni_dbus_dict_add_uint32(dict, "missed_max", lw->nsna.missed_max);
+		if (lw->nsna.missed > 0)
+			ni_dbus_dict_add_uint32(dict, "missed", lw->nsna.missed);
+		break;
+	case NI_TEAM_LINK_WATCH_TIPC:
+		if (lw->tipc.bearer)
+			ni_dbus_dict_add_string(dict, "bearer", lw->tipc.bearer);
+		break;
+	default:
+		return  FALSE;
+	}
+
+	return TRUE;
+}
+
+static dbus_bool_t
+__ni_objectmodel_team_link_watch_from_dict(ni_team_link_watch_t *lw, const ni_dbus_variant_t *dict,
+				DBusError *error, const ni_dbus_property_t *property)
+{
+	const char *string;
+	dbus_bool_t bvalue;
+	uint32_t value;
+
+	if (!lw || !dict || !error)
+		return FALSE;
+
+	switch(lw->type) {
+	case NI_TEAM_LINK_WATCH_ETHTOOL:
+		if (ni_dbus_dict_get_uint32(dict, "down_up", &value))
+			lw->ethtool.delay_up = value;
+		if (ni_dbus_dict_get_uint32(dict, "down_down", &value))
+			lw->ethtool.delay_down = value;
+		break;
+	case NI_TEAM_LINK_WATCH_ARP_PING:
+		if (ni_dbus_dict_get_string(dict, "source_host", &string))
+			ni_string_dup(&lw->arp.source_host, string);
+		if (ni_dbus_dict_get_string(dict, "target_host", &string))
+			ni_string_dup(&lw->arp.target_host, string);
+		if (ni_dbus_dict_get_uint32(dict, "interval", &value))
+			lw->arp.interval = value;
+		if (ni_dbus_dict_get_uint32(dict, "init_wait", &value))
+			lw->arp.init_wait = value;
+		if (ni_dbus_dict_get_bool(dict, "validate_active", &bvalue))
+			lw->arp.validate_active = bvalue;
+		if (ni_dbus_dict_get_bool(dict, "validate_inactive", &bvalue))
+			lw->arp.validate_inactive = bvalue;
+		if (ni_dbus_dict_get_bool(dict, "send_always", &bvalue))
+			lw->arp.send_always = bvalue;
+		if (ni_dbus_dict_get_uint32(dict, "missed_max", &value))
+			lw->arp.missed_max = value;
+		if (ni_dbus_dict_get_uint32(dict, "missed", &value))
+			 lw->arp.missed = value;
+		break;
+	case NI_TEAM_LINK_WATCH_NSNA_PING:
+		if (ni_dbus_dict_get_string(dict, "target_host", &string))
+			ni_string_dup(&lw->nsna.target_host, string);
+		if (ni_dbus_dict_get_uint32(dict, "interval", &value))
+			lw->nsna.interval = value;
+		if (ni_dbus_dict_get_uint32(dict, "init_wait", &value))
+			lw->nsna.init_wait = value;
+		if (ni_dbus_dict_get_uint32(dict, "missed_max", &value))
+			lw->nsna.missed_max = value;
+		if (ni_dbus_dict_get_uint32(dict, "missed", &value))
+			 lw->nsna.missed = value;
+		break;
+	case NI_TEAM_LINK_WATCH_TIPC:
+		if (ni_dbus_dict_get_string(dict, "bearer", &string))
+			ni_string_dup(&lw->tipc.bearer, string);
+		break;
+	default:
+		return  FALSE;
+	}
+
+	return TRUE;
+}
+
+/*
+ * Property link_watch
+ */
+static dbus_bool_t
+__ni_objectmodel_team_get_link_watch(const ni_dbus_object_t *object, const ni_dbus_property_t *property,
+				ni_dbus_variant_t *result, DBusError *error)
+{
+	const ni_team_t *team;
+	unsigned int i;
+
+	if (!(team = __ni_objectmodel_team_read_handle(object, error)))
+		return FALSE;
+
+	if (!team->link_watch.count)
+		return FALSE;
+
+	ni_dbus_variant_init_dict(result);
+	for (i = 0; i < team->link_watch.count; i++) {
+		ni_team_link_watch_t *lw = team->link_watch.data[i];
+		ni_dbus_variant_t *entry;
+		ni_dbus_variant_t *dict;
+		const char *name;
+
+		if (!(name = ni_team_link_watch_type_to_name(lw->type)))
+			continue;
+
+		entry = ni_dbus_dict_add(result, "watch");
+		ni_dbus_variant_init_struct(entry);
+		ni_dbus_struct_add_string(entry, name);
+		dict = ni_dbus_struct_add(entry);
+		ni_dbus_variant_init_dict(dict);
+		__ni_objectmodel_team_link_watch_to_dict(lw, dict, error, object, property);
+	}
+	return TRUE;
+}
+
+static dbus_bool_t
+__ni_objectmodel_team_set_link_watch(ni_dbus_object_t *object, const ni_dbus_property_t *property,
+				const ni_dbus_variant_t *argument, DBusError *error)
+{
+	const ni_dbus_variant_t *entry;
+	ni_team_t *team;
+	unsigned int i;
+
+	if (!(team = __ni_objectmodel_team_write_handle(object, error)))
+		return FALSE;
+
+	if (!ni_dbus_variant_is_dict(argument))
+		return FALSE;
+
+	i = 0; entry = NULL;
+	while ((entry = ni_dbus_dict_get_next(argument, "watch", entry))) {
+		const ni_dbus_variant_t *dict;
+		ni_team_link_watch_type_t type;
+		ni_team_link_watch_t *lw;
+		const char *name;
+
+		if (!ni_dbus_struct_get_string(entry, 0, &name)) {
+			dbus_set_error(error, DBUS_ERROR_INVALID_ARGS,
+					"bad array element %u value for property %s; missed subtype",
+					i, property->name);
+			return FALSE;
+		}
+
+		if (!ni_team_link_watch_name_to_type(name, &type)) {
+			dbus_set_error(error, DBUS_ERROR_INVALID_ARGS,
+					"bad array element %u for property %s; unsupported subtype %s",
+					i, property->name, name);
+			return FALSE;
+		}
+
+		if (!(dict = ni_dbus_struct_get(entry, 1))) {
+			dbus_set_error(error, DBUS_ERROR_INVALID_ARGS,
+					"missed team link_watch member dict in array element %u", i);
+			return FALSE;
+		}
+
+		if (!ni_dbus_variant_is_dict(dict)) {
+			dbus_set_error(error, DBUS_ERROR_INVALID_ARGS,
+					"team link_watch array element %u is not a dict", i);
+			return FALSE;
+		}
+
+		lw = ni_team_link_watch_new(type);
+		if (!__ni_objectmodel_team_link_watch_from_dict(lw, dict, error, property)) {
+			ni_team_link_watch_free(lw);
+			return FALSE;
+		}
+
+		ni_team_link_watch_array_append(&team->link_watch, lw);
+		i++;
+	}
+	return TRUE;
+}
+
 #define TEAM_HWADDR_PROPERTY(dbus_name, rw) \
 	__NI_DBUS_PROPERTY(DBUS_TYPE_ARRAY_AS_STRING DBUS_TYPE_BYTE_AS_STRING, \
 			dbus_name, __ni_objectmodel_team, rw)
@@ -487,6 +697,7 @@ __ni_objectmodel_team_set_runner(ni_dbus_object_t *object, const ni_dbus_propert
 
 static ni_dbus_property_t	ni_objectmodel_team_properties[] = {
 	TEAM_DICT_PROPERTY(runner, runner, RO),
+	TEAM_DICT_PROPERTY(link_watch, link_watch, RO),
 	TEAM_HWADDR_PROPERTY(address, RO),
 
 	{ NULL }
