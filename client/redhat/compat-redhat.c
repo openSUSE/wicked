9aa91a38d8f9b519036b2c58ed4dc99be7a31c60
Marius Tomaschewski <mt@suse.de>
2016-03-30 10:49:48 +0200

    bonding: fixed typo added in last commits

--- client/redhat/compat-redhat.c
+++ client/redhat/compat-redhat.c
@@ -340,9 +340,9 @@ try_bonding_slave(ni_sysconfig_t *sc, ni_compat_netdev_t *compat, ni_compat_netd
 	}
 
 	bonding = ni_netdev_get_bonding(master->dev);
-	if (ni_bonding_has_slave(bonding, var->value))
+	if (ni_bonding_has_slave(bonding, dev->name))
 		return TRUE;
-	return ni_bonding_add_slave(bonding, var->value) != NULL;
+	return ni_bonding_add_slave(bonding, dev->name) != NULL;
 }
 
 
