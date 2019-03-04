8a125d47a4e066e77cd560c8d814ac1495b2a0bb
Marius Tomaschewski <mt@suse.de>
2019-03-04 12:52:29 +0100

    daemons: change services to use systemd notify

--- nanny/main.c
+++ nanny/main.c
@@ -17,6 +17,9 @@
 #include <getopt.h>
 #include <limits.h>
 #include <errno.h>
+#ifdef HAVE_SYSTEMD_SD_DAEMON_H
+#include <systemd/sd-daemon.h>
+#endif
 
 #include <wicked/netinfo.h>
 #include <wicked/addrconf.h>
@@ -278,6 +281,12 @@ babysit(void)
 	}
 	else
 		ni_file_remove_recursively(ni_nanny_statedir());
+
+#ifdef HAVE_SYSTEMD_SD_DAEMON_H
+	if (opt_systemd) {
+		sd_notify(0, "READY=1");
+	}
+#endif
 
 	while (!ni_caught_terminal_signal()) {
 		long timeout = NI_IFWORKER_INFINITE_TIMEOUT;
