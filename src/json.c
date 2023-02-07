a74f27674877c9b0b97a4e8372f3c5ee58cecc94
Marius Tomaschewski <mt@suse.de>
2023-02-07 19:15:02 +0100

    json: fix token value leak in parse pair+initial

--- src/json.c
+++ src/json.c
@@ -1566,7 +1566,7 @@ ni_json_reader_parse_array(ni_json_reader_t *jr)
 		ni_json_reader_set_error(jr, "unexpected array token");
 		break;
 	}
-	ni_stringbuf_clear(&tokenValue);
+	ni_stringbuf_destroy(&tokenValue);
 }
 
 static void
@@ -1611,7 +1611,7 @@ ni_json_reader_parse_object(ni_json_reader_t *jr)
 		ni_json_reader_set_error(jr, "unexpected object token");
 		break;
 	}
-	ni_stringbuf_clear(&tokenValue);
+	ni_stringbuf_destroy(&tokenValue);
 }
 
 static void
@@ -1669,6 +1669,7 @@ ni_json_reader_parse_pair(ni_json_reader_t *jr)
 		ni_json_reader_set_error(jr, "unexpected object pair token");
 		break;
 	}
+	ni_stringbuf_destroy(&tokenValue);
 }
 
 static void
@@ -1718,6 +1719,7 @@ ni_json_reader_parse_initial(ni_json_reader_t *jr)
 		ni_json_reader_set_error(jr, "unexpected token");
 		break;
 	}
+	ni_stringbuf_destroy(&tokenValue);
 }
 
 static ni_json_t *
